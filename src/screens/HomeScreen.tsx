37309ea50e1aaf0819fb109174ccd9dd7e020bc1
charles reinaldo andres jara farfan <tj.charles.jara.f@upds.net.bo>
2023-04-18 16:19:30 -0400

    Primer Commit:subiendo updsAPP

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+
+
+export default HomeScreen;
